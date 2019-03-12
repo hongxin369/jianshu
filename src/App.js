4711cf694ccf9adec7dc78ccf59d3f609f5a1666
hongxin <hx754835899@gmail.com>
2019-03-13 01:38:51 +0800

    redux react-redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {Component, Fragment} from 'react';
 import Header from './common/header/index'
 import {Globalstyle} from  './static/iconFont/iconfont'
+import { Provider } from 'react-redux';
+import store from './store/index';
+
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Globalstyle/>
+        <Provider store={store}>
         <Header/>
+        </Provider>
       </Fragment>
     );
   }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,62 +1,64 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+
 import {HeaderWrapper, Logo, Nav, NavItem,NavSearch,Addition,Button,SearchWrapper} from "./style";
 import {CSSTransition} from 'react-transition-group'
 
-class Header extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      focused: false
-    };
-    this.handleInputFocus = this.handleInputFocus.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-  }
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo href='/'/>
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载APP</NavItem>
-          <NavItem className='right'>登录</NavItem>
-          <NavItem className='right'><i className="iconfont">&#xe636;</i>
-          </NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              timeout={200}
-              in={this.state.focused}
-              classNames='slide'
-            >
-              <NavSearch
-                className={this.state.focused ? 'focused' : ''}
-                onFocus={this.handleInputFocus}
-                onBlur={this.handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-            <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe62d;</i>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className='writing'><i className="iconfont">&#xe608;</i>写文章</Button>
-          <Button className='reg'>注册</Button>
-        </Addition>
-      </HeaderWrapper>
+const Header = (props) => {
+  return(
+    <HeaderWrapper>
+      <Logo href='/'/>
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载APP</NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <NavItem className='right'><i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            timeout={200}
+            in={props.focused}
+            classNames='slide'
+          >
+            <NavSearch
+              className={props.focused ? 'focused' : ''}
+              onFocus={props.handleInputFocus}
+              onBlur={props.handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe62d;</i>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='writing'><i className="iconfont">&#xe608;</i>写文章</Button>
+        <Button className='reg'>注册</Button>
+      </Addition>
+    </HeaderWrapper>
     )
-  }
-  handleInputFocus() {
-    this.setState(() => {
-      return {
-        focused: true
-      }
-    })
-  }
-  handleInputBlur() {
-    this.setState(() => {
-      return {
-        focused: false
-      }
-    })
-  }
+
 }
 
-export default Header;
+
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused
+  }
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      const action = {
+        type: 'search_focus'
+      };
+      dispatch(action)
+    },
+    handleInputBlur() {
+      const action = {
+        type: 'search_blur'
+      };
+      dispatch(action)
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
